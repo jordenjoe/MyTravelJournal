e87598002d037f7209ed7b14820731baba8718db
Jorden Jolley <94325574+jordenjoe@users.noreply.github.com>
2022-09-15 15:09:04 -0400

    fix

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,15 +1,16 @@
 import './App.css'
 import React from 'react'
 
+
 export default function Card(props) {
   console.log(props)
 
   return (
     <div>
     <div className="card">
-        <img className="card-img" src={`/src/assets/${props.item.imageUrl}`} />
+        <img className="card-img" src={`./src/assets/${props.item.imageUrl}`} />
       <div>
-        <p><span className="card-location"><span class="material-symbols-outlined pushpin">push_pin</span>{props.item.location} </span>
+        <p><span className="card-location"><span className="material-symbols-outlined pushpin">push_pin</span>{props.item.location} </span>
         <span className="card-link"><a href={props.item.googleMapsUrl}>View on Google Maps</a></span></p>
         <h1 className="card-title">{props.item.title}</h1>
         <p className="card-dates">{props.item.startDate}</p>
@@ -21,5 +22,3 @@ export default function Card(props) {
 
   )
 }
-
-//<img src="src/assets/croatia.png"/>
